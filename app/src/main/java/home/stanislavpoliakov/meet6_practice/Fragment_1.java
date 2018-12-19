a0bdf641a6897aaab7d598bff352067155025b07
Stanislav Poliakov <Stanislav.Poliakov@gmail.com>
2018-12-19 23:50:34 +0300

    Initial commit

---
+++ app/src/main/java/home/stanislavpoliakov/meet6_practice/Fragment_1.java
@@ -0,0 +1,30 @@
+package home.stanislavpoliakov.meet6_practice;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_1 extends Fragment {
+
+    public static Fragment_1 newInstance() {
+        Fragment_1 fragment = new Fragment_1();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/home/stanislavpoliakov/meet6_practice/Fragment_2.java
@@ -0,0 +1,29 @@
+package home.stanislavpoliakov.meet6_practice;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_2 extends Fragment {
+
+    public static Fragment_2 newInstance() {
+        Fragment_2 fragment = new Fragment_2();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
